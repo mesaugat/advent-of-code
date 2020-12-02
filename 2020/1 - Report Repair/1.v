c7c30c38c17ccf8eaa7f51396733bff8aa60eeda
Saugat Acharya <mesaugat@gmail.com>
2020-12-02 16:08:44 +0545

    Apparently solutions should not seg fault

--- 2020/1 - Report Repair/1.v
+++ 2020/1 - Report Repair/1.v
@@ -4,8 +4,8 @@ fn main() {
 	input := get_lines()
 	mut sum := 0
 	mut result := 0
-	for i := 0; i <= input.len; i++ {
-		for j := i + 1; j <= input.len - 1; j++ {
+	for i := 0; i < input.len; i++ {
+		for j := i + 1; j < input.len - 1; j++ {
 			sum = input[i].int() + input[j].int()
 			if sum == 2020 {
 				result = input[i].int() * input[j].int()
--- 2020/1 - Report Repair/2.v
+++ 2020/1 - Report Repair/2.v
@@ -4,9 +4,9 @@ fn main() {
 	input := get_lines()
 	mut sum := 0
 	mut result := 0
-	for i := 0; i <= input.len; i++ {
-		for j := i + 1; j <= input.len - 1; j++ {
-			for k := i + 2; k <= input.len - 2; k++ {
+	for i := 0; i < input.len; i++ {
+		for j := i + 1; j < input.len - 1; j++ {
+			for k := i + 2; k < input.len - 2; k++ {
 				sum = input[i].int() + input[j].int() + input[k].int()
 				if sum == 2020 {
 					result = input[i].int() * input[j].int() * input[k].int()
